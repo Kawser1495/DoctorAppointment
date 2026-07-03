0aba1f2fbd6c4fba9828ca7d4241bc71fe385fa6
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 02:24:56 +0600

    changes setting py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'accounts',
+    'patients',
+    'doctors',
+    'appointments',
+    'diagnostics',
+    'payments',
+    'reports',
+    'notifications',
+    'dashboard',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
