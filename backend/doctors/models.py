f074b014a9d2a2ead11eaefb9e97877039176fd1
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 15:47:58 +0600

    Doctor model py code modify

--- backend/doctors/models.py
+++ backend/doctors/models.py
@@ -21,3 +21,14 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    
+    
+class DoctorSchedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    day = models.CharField(max_length=20)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.doctor.user.username} - {self.day}"
