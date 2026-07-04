6988804c6f3648658d08321d9aa1c6c6442602a6
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 23:09:44 +0600

    Add dashboard views py code

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from appointments.models import Appointment
+from payments.models import Payment
+from diagnostics.models import TestBooking
 
-# Create your views here.
+class DashboardSummaryView(APIView):
+    def get(self, request):
+        data = {
+            "total_appointments": Appointment.objects.count(),
+            "total_payments": Payment.objects.count(),
+            "total_test_bookings": TestBooking.objects.count(),
+        }
+        return Response(data)
