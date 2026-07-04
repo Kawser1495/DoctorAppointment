bbc0730729b9e325cbaea833271839fbfd3f0120
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 23:07:07 +0600

    Add views py code

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Payment
+from .serializers import PaymentSerializer
 
-# Create your views here.
+class PaymentCreateView(generics.CreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
