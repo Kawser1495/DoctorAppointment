5eab78f35acd41113a9387e964a74e5e9a4e8f24
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 15:51:45 +0600

    Add appointment modl py coded

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from patients.models import PatientProfile
+from doctors.models import Doctor, TimeSlot
 
-# Create your models here.
+class Appointment(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    reason = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.patient.full_name} - {self.doctor.user.username}"
