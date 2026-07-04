735b8151e1ef5e06c068df732aa563f3ed827823
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 15:55:41 +0600

    modify doctor admin py code

--- backend/doctors/admin.py
+++ backend/doctors/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Department, Doctor
+from .models import Department, Doctor, DoctorSchedule, TimeSlot
 
 admin.site.register(Department)
 admin.site.register(Doctor)
+admin.site.register(DoctorSchedule)
+admin.site.register(TimeSlot)
