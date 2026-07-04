ad823038e373014c4a1767d04e2b364076ce375f
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 23:12:10 +0600

    Add config api connection

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,15 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Admin Panel
     path('admin/', admin.site.urls),
 
-    # Accounts API
     path('api/', include('accounts.urls')),
-
-    # Patients API
     path('api/patients/', include('patients.urls')),
-
-    # Doctors API
     path('api/doctors/', include('doctors.urls')),
+    path('api/appointments/', include('appointments.urls')),
+    path('api/tests/', include('diagnostics.urls')),
+    path('api/payments/', include('payments.urls')),
+    path('api/dashboard/', include('dashboard.urls')),
 ]
