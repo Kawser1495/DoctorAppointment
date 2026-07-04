65ec8c4db9f618a110b6daa70fee4999be11359d
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 17:07:54 +0600

    Add diagonstics more code

--- backend/diagnostics/models.py
+++ backend/diagnostics/models.py
@@ -6,3 +6,13 @@ class TestCategory(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class DiagnosticTest(models.Model):
+    category = models.ForeignKey(TestCategory, on_delete=models.CASCADE)
+    test_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.test_name
