43bc50ad7a1f4d49f8952a782bd656652732cd02
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 14:36:35 +0600

    Add Doctors code

--- backend/doctors/models.py
+++ backend/doctors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
