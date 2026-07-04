58886f70bdcd8c4e78415e0a30cebc97980002da
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 14:30:36 +0600

    add patient  code

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class PatientProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    blood_group = models.CharField(max_length=5)
+    address = models.TextField()
+    emergency_contact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.full_name
