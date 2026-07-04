914e3ba7562292cfeebae43e330a0b67c6f82770
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 23:01:06 +0600

    Appointment views py code

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Appointment
+from .serializers import AppointmentSerializer
 
-# Create your views here.
+class AppointmentCreateView(generics.CreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class AppointmentListView(generics.ListAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
