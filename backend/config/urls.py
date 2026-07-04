9fad34ab5bc932586f06ad670677712beb9c5a8f
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 22:37:43 +0600

    modify config urls py code

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Accounts API
     path('api/', include('accounts.urls')),
+
+    # Patients API
+    path('api/patients/', include('patients.urls')),
 ]
