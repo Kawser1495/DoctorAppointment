2779fe03a87dc06c2b1f6168b27dda7728294b9d
Kawser_Talukder <ktl149516@gmail.com>
2026-07-04 17:11:24 +0600

    Payment model py code add

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from patients.models import PatientProfile
 
-# Create your models here.
+class Payment(models.Model):
+    PAYMENT_METHODS = (
+        ('bkash', 'Bkash'),
+        ('nagad', 'Nagad'),
+        ('card', 'Card'),
+        ('cash', 'Cash'),
+    )
+
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    payment_status = models.CharField(max_length=20, default='paid')
+    transaction_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.transaction_id
