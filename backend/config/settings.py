f38895b755954a572c8a5e5e1278d34e25cbe246
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 19:18:40 +0600

    Create new folder util and add code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'django_filters',
 
     # Local Apps
     'accounts',
@@ -207,7 +208,18 @@ REST_FRAMEWORK = {
 
     ),
 
+    'DEFAULT_FILTER_BACKENDS': (
+
+        'django_filters.rest_framework.DjangoFilterBackend',
+
+        'rest_framework.filters.SearchFilter',
+
+        'rest_framework.filters.OrderingFilter',
+
+    ),
+
     'DEFAULT_PAGINATION_CLASS':
+
         'rest_framework.pagination.PageNumberPagination',
 
     'PAGE_SIZE': 10,
