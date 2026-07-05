d6c657272acf9a1ac14b050f8559cdd5c13bd1f8
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 19:08:03 +0600

    update appointment serializers code

--- backend/appointments/serializers.py
+++ backend/appointments/serializers.py
@@ -1,21 +1,37 @@
 from rest_framework import serializers
 from .models import Appointment
 
+
 class AppointmentSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = Appointment
-        fields = '__all__'
+
+        fields = "__all__"
 
     def validate(self, data):
-        appointment_date = data['appointment_date']
-        time_slot = data['time_slot']
+
+        doctor = data["doctor"]
+
+        slot = data["slot"]
+
+        appointment_date = data["appointment_date"]
 
         if Appointment.objects.filter(
-            appointment_date=appointment_date,
-            time_slot=time_slot
+
+            doctor=doctor,
+
+            slot=slot,
+
+            appointment_date=appointment_date
+
         ).exists():
+
             raise serializers.ValidationError(
-                "This slot is already booked."
+
+                "This appointment slot is already booked."
+
             )
 
         return data
