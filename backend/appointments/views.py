33278f8b2084f7c560f3c05272f5830e5620b13c
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 13:21:33 +0600

    Add appointment view new code

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -1,11 +1,33 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Appointment
 from .serializers import AppointmentSerializer
+
 
 class AppointmentCreateView(generics.CreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+
 class AppointmentListView(generics.ListAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
+
+class AppointmentCancelView(APIView):
+    def patch(self, request, pk):
+        try:
+            appointment = Appointment.objects.get(id=pk)
+            appointment.status = "cancelled"
+            appointment.save()
+
+            return Response({
+                "message": "Appointment cancelled successfully"
+            })
+
+        except Appointment.DoesNotExist:
+            return Response({
+                "error": "Appointment not found"
+            })
