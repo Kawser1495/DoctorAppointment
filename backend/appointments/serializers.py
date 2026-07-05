b9b0c2dce774942824897d63a98c01cad7ec2b27
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 13:14:41 +0600

    Duplicate slot prevention add

--- backend/appointments/serializers.py
+++ backend/appointments/serializers.py
@@ -5,3 +5,17 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
+
+    def validate(self, data):
+        appointment_date = data['appointment_date']
+        time_slot = data['time_slot']
+
+        if Appointment.objects.filter(
+            appointment_date=appointment_date,
+            time_slot=time_slot
+        ).exists():
+            raise serializers.ValidationError(
+                "This slot is already booked."
+            )
+
+        return data
