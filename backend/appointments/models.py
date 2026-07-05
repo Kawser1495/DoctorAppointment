b6c57d8e1dd78e7de01e0a1f1a8d826feab10ec2
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 14:10:42 +0600

    Modify model.py code in appointment

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from patients.models import PatientProfile
+from patients.models import PatientProfile, FamilyMember
 from doctors.models import Doctor, TimeSlot
 
 class Appointment(models.Model):
+
     STATUS_CHOICES = (
         ('pending', 'Pending'),
         ('confirmed', 'Confirmed'),
@@ -11,11 +12,28 @@ class Appointment(models.Model):
     )
 
     patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+
+    family_member = models.ForeignKey(
+        FamilyMember,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="appointments"
+    )
+
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
     slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+
     appointment_date = models.DateField()
+
     reason = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
 
     def __str__(self):
         return f"{self.patient.full_name} - {self.doctor.user.username}"
