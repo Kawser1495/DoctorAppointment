6cc2378da840c9cefca339daed5813b8b745a172
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 14:19:12 +0600

    modify doctors views code

--- backend/doctors/views.py
+++ backend/doctors/views.py
@@ -1,11 +1,22 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
+
 from .models import Doctor, Department
 from .serializers import DoctorSerializer, DepartmentSerializer
+
 
 class DoctorListView(generics.ListAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
+
 class DepartmentListView(generics.ListAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+
+
+class DoctorSearchView(generics.ListAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['specialization']
