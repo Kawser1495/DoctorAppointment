762f9c40f5c251414eaccbe69c60bf3325d65d7c
Kawser_Talukder <ktl149516@gmail.com>
2026-07-05 13:53:16 +0600

    Add Appointment urls py code

--- backend/appointments/urls.py
+++ backend/appointments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AppointmentCreateView, AppointmentListView
+from .views import AppointmentCreateView, AppointmentListView, AppointmentCancelView
 
 urlpatterns = [
     path('book/', AppointmentCreateView.as_view(), name='book-appointment'),
     path('list/', AppointmentListView.as_view(), name='appointment-list'),
+    path('cancel/<int:pk>/', AppointmentCancelView.as_view(), name='cancel-appointment'),
 ]
