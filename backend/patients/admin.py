060bb3824003b82310bfe62c11d26fab2b0be91e
Kawser_Talukder <ktl149516@gmail.com>
2026-07-13 12:17:29 +0600

    Add patients admin py code

--- backend/patients/admin.py
+++ backend/patients/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
 from .models import PatientProfile, FamilyMember
 
-admin.site.register(PatientProfile)
-admin.site.register(FamilyMember)
+
+@admin.register(PatientProfile)
+class PatientAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "full_name",
+        "phone",
+        "gender",
+        "blood_group",
+    )
+
+    search_fields = (
+        "full_name",
+        "phone",
+    )
+
+    list_filter = (
+        "gender",
+        "blood_group",
+    )
+
+
+@admin.register(FamilyMember)
+class FamilyMemberAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "patient",
+        "full_name",
+        "relationship",
+        "phone",
+    )
+
+    search_fields = (
+        "full_name",
+    )
