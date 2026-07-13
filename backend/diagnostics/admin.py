7efcb4f8879af1b4981d78b86281c8c95da0da5e
Kawser_Talukder <ktl149516@gmail.com>
2026-07-13 12:20:01 +0600

    Add diagonstics admin py code

--- backend/diagnostics/admin.py
+++ backend/diagnostics/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import TestCategory, DiagnosticTest, TestBooking
 
+
 admin.site.register(TestCategory)
 admin.site.register(DiagnosticTest)
 admin.site.register(TestBooking)
