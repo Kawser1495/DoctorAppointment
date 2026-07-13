51fd4ec9218154fafc1e3886f003ccab96ac0be0
Kawser_Talukder <ktl149516@gmail.com>
2026-07-13 12:16:02 +0600

    Add doctors admin py code

--- backend/doctors/admin.py
+++ backend/doctors/admin.py
@@ -1,7 +1,65 @@
 from django.contrib import admin
 from .models import Department, Doctor, DoctorSchedule, TimeSlot
 
-admin.site.register(Department)
-admin.site.register(Doctor)
-admin.site.register(DoctorSchedule)
-admin.site.register(TimeSlot)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "department",
+        "specialization",
+        "consultation_fee",
+    )
+
+    search_fields = (
+        "user__username",
+        "specialization",
+    )
+
+    list_filter = (
+        "department",
+    )
+
+
+@admin.register(DoctorSchedule)
+class DoctorScheduleAdmin(admin.ModelAdmin):
+    list_display = (
+        "doctor",
+        "day",
+        "start_time",
+        "end_time",
+    )
+
+    list_filter = (
+        "day",
+    )
+
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = (
+        "doctor",
+        "date",
+        "start_time",
+        "end_time",
+        "is_booked",
+    )
+
+    list_filter = (
+        "is_booked",
+        "date",
+    )
