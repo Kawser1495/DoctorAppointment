edae083f826e9e09594444246719d0f1a91d7a1e
Kawser_Talukder <ktl149516@gmail.com>
2026-07-15 23:09:50 +0600

    Modify Dashboard code

--- frontend/src/components/DashboardCard.jsx
+++ frontend/src/components/DashboardCard.jsx
@@ -1,26 +1,72 @@
 import React from "react";
 
-function DashboardCard({ title, value }) {
+import "./DashboardCard.css";
 
-    return (
+function DashboardCard({
+
+    title,
+
+    value,
+
+    color,
+
+    icon
+
+}){
+
+    return(
 
         <div
+
+            className="dashboard-card"
+
             style={{
-                background: "white",
-                padding: "20px",
-                borderRadius: "12px",
-                boxShadow: "0px 2px 8px rgba(0,0,0,.1)",
-                width: "220px",
+
+                borderLeft:`6px solid ${color}`
+
             }}
+
         >
 
-            <h5>{title}</h5>
+            <div className="card-top">
 
-            <h2>{value}</h2>
+                <div>
+
+                    <h5>
+
+                        {title}
+
+                    </h5>
+
+                    <h2>
+
+                        {value}
+
+                    </h2>
+
+                </div>
+
+                <div
+
+                    className="card-icon"
+
+                    style={{
+
+                        color:color
+
+                    }}
+
+                >
+
+                    {icon}
+
+                </div>
+
+            </div>
 
         </div>
 
-    );
+    )
 
 }
 
