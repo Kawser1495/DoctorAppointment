a273ad91ab80ff1739ae23d655e01862694e0373
Kawser_Talukder <ktl149516@gmail.com>
2026-07-15 18:12:24 +0600

    Sidebar code add

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,41 @@
-export default function Sidebar(){
+import React from "react";
 
-    return(
+function Sidebar() {
 
-        <div>
+    return (
 
-            Sidebar
+        <div
+            style={{
+                width: "250px",
+                height: "100vh",
+                background: "#1f2937",
+                color: "white",
+                padding: "20px",
+            }}
+        >
+
+            <h2>Menu</h2>
+
+            <hr />
+
+            <p>Dashboard</p>
+
+            <p>Book Appointment</p>
+
+            <p>My Appointments</p>
+
+            <p>Doctors</p>
+
+            <p>Medical Reports</p>
+
+            <p>Payments</p>
+
+            <p>Logout</p>
 
         </div>
 
-    )
+    );
 
 }
+
+export default Sidebar;
