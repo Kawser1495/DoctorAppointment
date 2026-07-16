6353ec09bb1d986f295d9bf6efd872e5a25877e3
Kawser_Talukder <ktl149516@gmail.com>
2026-07-16 13:55:52 +0600

    Modify Dashboard api code

--- frontend/src/api/dashboardApi.js
+++ frontend/src/api/dashboardApi.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL: "http://127.0.0.1:8000/api"
-
+    baseURL: "http://127.0.0.1:8000/api",
 });
 
-export const getDashboardData = () =>
+// প্রতিটি Request-এর সাথে JWT Token পাঠাবে
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access");
 
-    API.get("/dashboard/");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export const getDashboardData = () => API.get("/dashboard/");
+
+export default API;
