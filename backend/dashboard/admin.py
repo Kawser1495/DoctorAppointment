0dd6f3d4896d91e80d311af7de06ac80692aec36
Kawser_Talukder <ktl149516@gmail.com>
2026-07-18 23:32:12 +0600

    Change dashboard admin py code

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from .models import DashboardStatistic
 
-
-@admin.register(DashboardStatistic)
-class DashboardStatisticAdmin(admin.ModelAdmin):
-
-    list_display = (
-        "total_patients",
-        "total_doctors",
-        "total_appointments",
-        "total_departments",
-        "total_payments",
-        "last_updated",
-    )
-
-    readonly_fields = (
-        "last_updated",
-    )
-
-    search_fields = ()
-
-    ordering = (
-        "-last_updated",
-    )
+# Register your dashboard models here.
