c3b25c21a678342ff701cd308ec2d5780051eadc
Kawser_Talukder <ktl149516@gmail.com>
2026-08-05 08:48:07 +0600

    Update AuthContext and App.jsx code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "../context/AuthContext";
 
 function App() {
-    return <AppRoutes />;
+
+    return (
+
+        <AuthProvider>
+
+            <AppRoutes />
+
+        </AuthProvider>
+
+    );
+
 }
 
 export default App;
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,77 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+
+// ==========================================
+// Create Context
+// ==========================================
 
 const AuthContext = createContext();
+
+// ==========================================
+// Auth Provider
+// ==========================================
+
+export function AuthProvider({ children }) {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const token = localStorage.getItem("access");
+
+        if (token) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+
+        setLoading(false);
+
+    }, []);
+
+    // ==========================================
+    // Login
+    // ==========================================
+
+    const login = (access, refresh) => {
+
+        localStorage.setItem("access", access);
+        localStorage.setItem("refresh", refresh);
+
+        setIsAuthenticated(true);
+
+    };
+
+    // ==========================================
+    // Logout
+    // ==========================================
+
+    const logout = () => {
+
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+
+        setIsAuthenticated(false);
+
+    };
+
+    return (
+
+        <AuthContext.Provider
+            value={{
+                isAuthenticated,
+                loading,
+                login,
+                logout,
+            }}
+        >
+
+            {children}
+
+        </AuthContext.Provider>
+
+    );
+
+}
 
 export default AuthContext;
